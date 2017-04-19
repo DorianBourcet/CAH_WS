6bf57979717cc9c7302d7eab2e38e71d19eaf9fe
jycy <jcf.860@gmail.com>
2017-04-19 02:51:09 -0400

    changed else if to switch for command

--- CAH_WS/web/resources/websocket.js
+++ CAH_WS/web/resources/websocket.js
@@ -20,12 +20,13 @@
         
         var response = event.data;
         console.log(response.trim());
-        if(event.data === "_success_login") {$('#Login-modal').modal('hide');}
-        else if (event.data ==="_error_user") {$('#user').css("border-color","red");}
-        else if (event.data ==="_error_pwd") {$('#pass').css("border-color","red");}
-        else if (event.data == "_display_modal") {$('#Login-modal').modal('show');}
-        else document.getElementById('messages').innerHTML += '<br />'
-                + event.data;
+        switch(event.data){
+            case "_success_login": $('#Login-modal').modal('hide');break;
+            case "_error_user": $('#user').css("border-color","red");break;
+            case "_error_pwd": $('#pass').css("border-color","red");break;
+            case "_display_modal": $('#Login-modal').modal('show');break;
+            default: document.getElementById('messages').innerHTML = event.data;
+        }
     }
 
     function onOpen(event) {
