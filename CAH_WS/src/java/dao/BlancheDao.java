4c1f793470ef2ab8f9544daf51051cf3dd6a5b42
Dorian Bourcet <soixante6dollars@gmail.com>
2017-05-11 13:52:23 -0400

    Add cards DAOs with findAll method
    Signed-off-by: Dorian Bourcet <soixante6dollars@gmail.com>

---
+++ CAH_WS/src/java/dao/BlancheDao.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import com.atoudeft.jdbc.dao.Dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Blanche;
+
+/**
+ *
+ * @author soixa
+ */
+public class BlancheDao extends Dao<Blanche> {
+
+    public BlancheDao(Connection cnx) {
+        super(cnx);
+    }
+
+    @Override
+    public boolean create(Blanche x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Blanche read(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Blanche x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(Blanche x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Blanche> findAll() {
+        List<Blanche> liste = new ArrayList();
+        PreparedStatement stm = null;
+        try 
+            {
+                stm = cnx.prepareStatement("select * from cartes_blanches");
+                ResultSet r = stm.executeQuery();
+                while (r.next())
+                {
+                    Blanche b = new Blanche(r.getString("Texte"));
+                    liste.add(b);
+                }
+                r.close();
+                stm.close();
+            }
+            catch (SQLException exp) {
+            }
+            return liste;
+    }
+    
+}
---
+++ CAH_WS/src/java/dao/NoireDao.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import com.atoudeft.jdbc.dao.Dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Noire;
+
+/**
+ *
+ * @author soixa
+ */
+public class NoireDao extends Dao<Noire> {
+
+    public NoireDao(Connection cnx) {
+        super(cnx);
+    }
+
+    @Override
+    public boolean create(Noire x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Noire read(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Noire x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(Noire x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Noire> findAll() {
+        List<Noire> liste = new ArrayList();
+        PreparedStatement stm = null;
+        try 
+            {
+                stm = cnx.prepareStatement("select * from cartes_noires");
+                ResultSet r = stm.executeQuery();
+                while (r.next())
+                {
+                    Noire n = new Noire(r.getString("Texte"),r.getInt("Piger"));
+                    liste.add(n);
+                }
+                r.close();
+                stm.close();
+            }
+            catch (SQLException exp) {
+            }
+            return liste;
+    }
+    
+}
